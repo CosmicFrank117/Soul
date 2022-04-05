e3a31fd3916338f607e9ba5298bac047e32ffa1c
CosmicFrank117 <frank58928@yahoo.com>
2022-04-04 23:14:30 -0700

    Added enemy soul, changed soul collision name

---
+++ GH 'Cool' Game/Assets/Scripts/SoulCollision.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulCollision : MonoBehaviour
+{
+        [SerializeField] int amountToIncrease = 1;
+
+        ScoreManager scoreManager;
+        PlayerSoulChanger playerSoulChanger;
+    
+    void Start() 
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+        playerSoulChanger = FindObjectOfType<PlayerSoulChanger>();
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {
+        scoreManager.IncreaseScore(amountToIncrease);
+        
+        playerSoulChanger.IncreaseLightSizeAndBrighness();
+        playerSoulChanger.IncreaseSphereSize();
+        
+        Destroy(gameObject);
+    }
+}
